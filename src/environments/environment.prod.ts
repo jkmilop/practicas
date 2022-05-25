ce35306d4e3078dd3f66371c592b7a86210b8f48
DESKTOP-KV9U18O\Cwng <jkmilop@gmail.com>
2022-05-25 01:08:14 -0500

    fanzi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    apiKey: "AIzaSyAKdccN2p7dlVHg4154ilPtNnFUVAU3My0",
+    authDomain: "practiweb-usb-x.firebaseapp.com",
+    projectId: "practiweb-usb-x",
+    storageBucket: "practiweb-usb-x.appspot.com",
+    messagingSenderId: "98813543689",
+    appId: "1:98813543689:web:022cc2b3ca6b5a19cdbfd8",
+    measurementId: "G-DSQND419ET"
+  },
   production: true
 };
